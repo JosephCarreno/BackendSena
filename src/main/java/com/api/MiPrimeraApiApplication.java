f584afcee2b1d8e03957321f087ac9f62f9aabc6
Joseph Carreño <141787641+Joseph234c@users.noreply.github.com>
2024-10-03 12:01:21 -0500

    version 1.1 de backend

--- src/main/java/com/api/MiPrimeraApiApplication.java
+++ src/main/java/com/api/MiPrimeraApiApplication.java
@@ -14,18 +14,6 @@ public class MiPrimeraApiApplication {
 		SpringApplication.run(MiPrimeraApiApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedMethods("HEAD","GET","PUT","POST","DELETE","PATCH");
-				}
-			};
-		}
-	}s
 
 }
 
